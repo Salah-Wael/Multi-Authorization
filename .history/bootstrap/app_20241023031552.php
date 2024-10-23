56f706ce7eff3c2b9d39faa3b3484920ea98ca42
Salah <sasa.wael2016@gmail.com>
2024-10-23 10:48:48 +0300

    add admin guard to the system, active his auth methods, and customize his views

---
+++ .history/bootstrap/app_20241023031552.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+
+
+        $middleware->add('auth', \App\Http\Middleware\Authenticate::class);
+
+        $middleware->add('auth.basic', \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class);
+
+        $middleware->add('auth.session', \Illuminate\Session\Middleware\AuthenticateSession::class);
+        
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
